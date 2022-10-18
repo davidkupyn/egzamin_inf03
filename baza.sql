73a657d0570e182739a7cc1e5dea745cc83bb57c
davidkupyn <71174027+davidkupyn@users.noreply.github.com>
2022-10-18 16:20:33 +0200

    -

--- baza.sql
+++ baza.sql
@@ -12,48 +12,46 @@ CREATE TABLE restaurant (
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     personal_id VARCHAR(11) NOT NULL,
-    PRIMARY KEY (id),
 );
+
 CREATE TABLE restaurant_employee (
-    restaurant_id INT NOT NULL,
-    employee_id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    restaurant_id INT NOT NULL PRIMARY KEY,
+    employee_id INT NOT NULL PRIMARY KEY,
     role_id INT NOT NULL,
-    PRIMARY KEY (restaurant_id, employee_id),
     FOREIGN KEY (restaurant_id) REFERENCES restaurant(id),
     FOREIGN KEY (employee_id) REFERENCES employee(id),
     FOREIGN KEY (role_id) REFERENCES roles(id)
 );
 
 CREATE TABLE reservation (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     phone VARCHAR(12) NOT NULL,
     surname VARCHAR(50) NOT NULL,
     restaurant_id INT NOT NULL,
     reservation_date DATETIME NOT NULL,
     guests INT NOT NULL,
-    PRIMARY KEY (id),
     FOREIGN KEY (restaurant_id) REFERENCES restaurant(id)
 );
 CREATE TABLE meal_type (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id)
 );
 CREATE TABLE meals (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     meal_type_id INT NOT NULL,
+    restaurant_id INT NOT NULL,
     base_price DECIMAL(5,2) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (meal_type_id) REFERENCES meal_type(id)
+    FOREIGN KEY (meal_type_id) REFERENCES meal_type(id),
+    FOREIGN KEY (restaurant_id) REFERENCES restaurant(id)
 );
